4e588f84368ffb9f49a8258ac85d5871f47a18bc
Chris Martin <martinc1701@gmail.com>
2014-03-26 15:23:17 -0600

    tweaked the download a bit

--- src/download.cc
+++ src/download.cc
@@ -20,12 +20,12 @@ Download::Download(const QUrl &url, QString saveDir, QObject *parent)
 
 	// Don't overwrite anything - come up with a unique filename by
 	// appending ".#" to the filename.
-	if (QFile::exists(m_filename)) {
+	/*if (QFile::exists(m_filename)) {
 		int i = 0;
 		m_filename += '.';
 		while (QFile::exists(m_filename + QString::number(i))) ++i;
 		m_filename += QString::number(i);
-	}
+		}*/
 
 	m_output.setFileName(saveDir);// + "/" + m_filename);
 	// Open the output file. If we can't, then throw an exception.
@@ -69,9 +69,11 @@ void Download::m_downloadProgress(qint64 recv, qint64 total) {
 	m_curKbytes = (qreal)recv / 1024.0;
 	m_speedKbps = m_curKbytes * 1000.0 / m_time.elapsed();
 	//XXX
-	cout << qPrintable(m_filename) << ": "
+	/*
+	  cout << qPrintable(m_filename) << ": "
 		 << fixed << setprecision(1)
 		 << m_curKbytes << "kb / " << m_totalKbytes << "kb (" << m_speedKbps << "kb/s)\n";
+	*/
 
 	emit progress(m_curKbytes, m_speedKbps);
 }
--- src/downloadmanager.cc
+++ src/downloadmanager.cc
@@ -12,44 +12,47 @@ using std::cout;
 using std::endl;
 
 DownloadManager::DownloadManager(QWidget *parent) : QWidget(parent){
-    setWindowTitle("Download Manager");
-    m_title = new QLabel("Current Downloads:");
-    m_hideBtn = new QPushButton("Hide");
+	setWindowTitle("Download Manager");
+	setMinimumWidth(320);
+	//setMinimumHeight(240);
+	
+	m_title = new QLabel("Current Downloads:");
+	m_hideBtn = new QPushButton("Hide");
+	
+	m_downloadArea = new QVBoxLayout();
+	
+	connect(m_hideBtn, SIGNAL(clicked()), this, SLOT(hide()));
+	
+	QVBoxLayout *right = new QVBoxLayout();
+	right->addWidget(m_hideBtn);
+	
+	QHBoxLayout *bottom = new QHBoxLayout();
+	bottom->addLayout(m_downloadArea);
+	bottom->addLayout(right);
 
-    m_downloadArea = new QVBoxLayout();
+	QVBoxLayout *layout = new QVBoxLayout();
+	layout->addWidget(m_title);
+	layout->addLayout(bottom);
 
-    connect(m_hideBtn, SIGNAL(clicked()), this, SLOT(hide()));
-
-    QVBoxLayout *right = new QVBoxLayout();
-    right->addWidget(m_hideBtn);
-
-    QHBoxLayout *bottom = new QHBoxLayout();
-    bottom->addLayout(m_downloadArea);
-    bottom->addLayout(right);
-
-    QVBoxLayout *layout = new QVBoxLayout();
-    layout->addWidget(m_title);
-    layout->addLayout(bottom);
-
-    this->setLayout(layout);
+	this->setLayout(layout);
 
 }
 
 void DownloadManager::downloadRequested(const QUrl &url){
-    QString path = url.path();
-    QString fileName = QFileDialog::getSaveFileName(this, tr("Save File"), QFileInfo(path).fileName());
-    cout<<"filename="<<qPrintable(fileName)<<endl;
-    m_downloads.push_back(new Download(url, fileName));
-    m_downloadArea->addWidget(new DownloadInfo(m_downloads[m_downloads.size() - 1]));
-    this->show();
+	QString path = url.path();
+	QString fileName = QFileDialog::getSaveFileName(this, tr("Save File"), QFileInfo(path).fileName());
+	cout<<"filename="<<qPrintable(fileName)<<endl;
+	m_downloads.push_back(new Download(url, fileName));
+	m_downloadArea->addWidget(new DownloadInfo(m_downloads[m_downloads.size() - 1]));
+	this->show();
 }
 
 void DownloadManager::paintEvent(QPaintEvent *event){
-    QPainter painter(this);
-    painter.setRenderHint(QPainter::Antialiasing, true);
-    painter.setWindow(-50, -50, 100, 100);
-\
-    QWidget::paintEvent(event);
+	QPainter painter(this);
+	painter.setRenderHint(QPainter::Antialiasing, true);
+	painter.setWindow(-50, -50, 100, 100);
+	\
+	QWidget::paintEvent(event);
 }
 
 
